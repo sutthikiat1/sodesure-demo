300e19811806143192a9db3755a2327b25a4054a
Sutthikiat Phogsakornmetha <sutthikiat.ph@gmail.com>
2026-01-28 12:08:00 +0700

    onDragStart hide pin on map

--- src/components/Screen/MapScreen/index.tsx
+++ src/components/Screen/MapScreen/index.tsx
@@ -383,7 +383,7 @@ function MapScreen() {
               center={userLocation || defaultCenter}
               zoom={14}
               onLoad={onMapLoad}
-              onClick={() => setTimeout(() => setSelectedPlace(null), 300)}
+              onDragStart={() => setSelectedPlace(null)}
               options={{
                 disableDefaultUI: true,
                 zoomControl: false,
